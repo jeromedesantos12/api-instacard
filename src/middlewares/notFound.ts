2e64aace7753595d5f6fb043eced319bceda3bb6
Jeremy Santoso <jeremysantoso98@gmail.com>
2025-10-22 22:50:14 +0700

    Change notFound middleware to throw error

--- src/middlewares/notFound.ts
+++ src/middlewares/notFound.ts
@@ -4,7 +4,7 @@ const version = process.env.API_VERSION;
 
 export function notFound(req: Request, res: Response, next: NextFunction) {
   try {
-    res.send(`API version ${version}`);
+    throw appError("Route Not Found!", 404);
   } catch (err) {
     next(err);
   }
